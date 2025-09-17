7d9fc7e3794e6a1d59a2abca09dc0018d6b2bb51
Maharshi Patel <new.email@example.com>
2025-09-17 14:43:04 +0530

    Checkpoint for debugging (need debugging)

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,26 +1,43 @@
 const jwt = require('jsonwebtoken');
-
-
-
 module.exports = (req, res , next) => {
-
     try{
 
-        const token = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        //temp
+        console.log('Authorization Header:', req.headers.authorization);  // Temporary debug log
 
-        req.body.userId = decodedToken.userId; 
-        next();
-        // const userId = decodedToken.userId;
+    if (!req.headers.authorization) {
+      return res.status(401).send({
+        message: "Authorization header missing",
+        success: false
+      });
+    }
+    const token = req.headers.authorization.split(" ")[1];
 
-
+    // temp
+    if (!token) {
+      return res.status(401).send({
+        message: "Invalid token format (use Bearer <token>)",
+        success: false
+      });
     }
 
 
 
 
+    //    const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);  
 
 
+        // console.log('Decoded Token:', decodedToken);  // Temporary debug log
+
+
+        req.userId = decodedToken.userId; // Corrected to match token payload
+
+        next();
+        // const userId = decodedToken.userId;
+
+
+    }
     catch(err){
         res.send({
             message : err ,
