c5ca2e6915cda803142c60b0970d211706be4b04
Maharshi Patel <new.email@example.com>
2025-09-17 10:06:12 +0530

    Added User Login Route

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 router.post("/signup", async(req, res) => {
@@ -41,8 +42,50 @@ router.post("/signup", async(req, res) => {
 
     });
   }
-  
+
 }); 
+
+
+router.post("/login", async(req, res) => {
+  try{
+    const user = await User.findOne({ email: req.body.email });
+    if(!user) {
+      return res.status(404).send({
+        message: "User not found",
+        success: false
+      });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if(!isMatch) {
+      return res.status(400).send({
+        message: "Incorrect password",
+        success: false
+      });
+    }
+
+    //generate jwt token
+    const token = jwt.sign ({userID : user._id}, process.env.SECRET_KEY, {expiresIn: "1d"});
+
+      res.send({
+        message: "Login successful",
+        success: true,
+        token: token
+      });
+
+    // res.status(200).send({
+    //   message: "Login successful",
+    //   success: true,
+    //   data: user
+    // });
+  }
+  catch(error) {
+    res.send({
+      message: error.message,
+      success: false
+    });
+  }
+});
+
 
 
 module.exports = router;
